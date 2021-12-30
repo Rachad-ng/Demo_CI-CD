6dd1f55ea3dfb49994b8bc0c33d9a0e92a01ef3d
Rachad-ng <62219529+Rachad-ng@users.noreply.github.com>
2021-12-30 20:44:07 +0100

    Initialisation du projet

---
+++ src/main/java/com/negra/FirstProjectGitApplication.java
@@ -0,0 +1,13 @@
+package com.negra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/negra/FirstProjectGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.negra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
